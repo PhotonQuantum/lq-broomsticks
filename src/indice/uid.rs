67572f237df785305ca2599f3a9f73960e493471
PhotonQuantum <self@lightquantum.me>
2020-12-11 06:04:45 +0800

    initial implementation of uid.

---
+++ src/indice/uid.rs
@@ -0,0 +1,44 @@
+use crate::ast::*;
+use std::collections::HashMap;
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct UIDGenerator {
+    count: usize
+}
+impl UIDGenerator {
+    pub fn default() -> Self {
+        UIDGenerator{count: 0}
+    }
+    pub fn next(&mut self) -> usize {
+        self.count += 1;
+        self.count
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+pub struct UID {
+    pub name: char,
+    pub uid: usize
+}
+
+impl IDType for UID {
+    fn get_name(&self) -> char {
+        self.name
+    }
+}
+
+pub fn to_uid(term: &Term<BareID>, uid_generator: &mut UIDGenerator) -> Term<UID> {
+    _to_uid(term, uid_generator, HashMap::default())
+}
+
+fn _to_uid(term: &Term<BareID>, uid_generator: &mut UIDGenerator, mut free_vars: HashMap<char, usize>) -> Term<UID> {
+    match term {
+        Var(chr) => Var(UID{name: *chr, uid: if free_vars.contains_key(chr) {free_vars[chr]} else {uid_generator.next()}}),
+        Abs(bound, term) => {
+            let bound_id = uid_generator.next();
+            free_vars.insert(*bound, bound_id);
+            Abs(UID{name: *bound, uid: bound_id}, box _to_uid(term, uid_generator, free_vars))
+        },
+        App(lhs, rhs) => App(box _to_uid(lhs, uid_generator, free_vars.clone()), box _to_uid(rhs, uid_generator, free_vars))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,22 @@ extern crate pest_derive;
 
 mod ast;
 mod parser;
+mod indice;
 
 use crate::parser::parse;
+use indice::uid::{to_uid, UIDGenerator};
 
 fn main() {
+    test_uid("λf.(λx.f (x x)) (λx.f (x x))");
+    test_uid("λx.xx");
 }
 
+fn test_uid(expr: &str) {
+    let expr = parse(expr).unwrap();
+    println!("{:?}", expr);
+
+    let mut gen = UIDGenerator::default();
+    let uid_expr = to_uid(&expr, &mut gen);
+    println!("{:?}", uid_expr);
+    println!("{}", uid_expr.to_bare());
+}
