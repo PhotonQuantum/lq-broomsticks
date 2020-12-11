750fc0299bde0b4a17f0a62e9318e9dbac2dde25
PhotonQuantum <self@lightquantum.me>
2020-12-12 03:51:58 +0800

    fix(uid): incorrect uid assignment for free vars

--- src/indice/uid.rs
+++ src/indice/uid.rs
@@ -3,11 +3,12 @@ use std::collections::HashMap;
 
 #[derive(Copy, Clone, Eq, PartialEq)]
 pub struct UIDGenerator {
-    count: usize
+    count: usize,
 }
+
 impl UIDGenerator {
     pub fn default() -> Self {
-        UIDGenerator{count: 0}
+        UIDGenerator { count: 0 }
     }
     pub fn next(&mut self) -> usize {
         self.count += 1;
@@ -28,12 +29,27 @@ impl IDType for UID {
 }
 
 pub fn to_uid(term: &Term<BareID>, uid_generator: &mut UIDGenerator) -> Term<UID> {
-    _to_uid(term, uid_generator, HashMap::default())
+    _to_uid(term, uid_generator, HashMap::default(), HashMap::default()).0
 }
 
-fn _to_uid(term: &Term<BareID>, uid_generator: &mut UIDGenerator, mut free_vars: HashMap<char, usize>) -> Term<UID> {
+fn _to_uid(
+    term: &Term<BareID>,
+    uid_generator: &mut UIDGenerator,
+    mut free_vars: HashMap<char, usize>,
+    mut bound_vars: HashMap<char, usize>,
+) -> (Term<UID>, HashMap<char, usize>) {
     match term {
-        Var(chr) => Var(UID{name: *chr, uid: if free_vars.contains_key(chr) {free_vars[chr]} else {uid_generator.next()}}),
+        Var(chr) => {
+            let uid = if free_vars.contains_key(chr) {
+                free_vars[chr]
+            } else if bound_vars.contains_key(chr) {
+                bound_vars[chr]
+            } else {
+                free_vars.insert(*chr, uid_generator.next());
+                free_vars[chr]
+            };
+            (Var(UID { name: *chr, uid }), free_vars)
+        }
         Abs(bound, term) => {
             let bound_id = uid_generator.next();
             free_vars.insert(*bound, bound_id);
